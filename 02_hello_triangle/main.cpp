f817832f643ac4991e8ae3b9cc0a44ff549a9f04
tuckidart <tiagoduarte6@gmail.com>
2018-05-15 20:36:21 -0300

    cara, ta funcionando delicia

--- 02_hello_triangle/main.cpp
+++ 02_hello_triangle/main.cpp
@@ -37,40 +37,45 @@ int main() {
 	start_gl();
 
 	// tell GL to only draw onto a pixel if the shape is closer to the viewer
-	glEnable( GL_DEPTH_TEST ); // enable depth-testing
-	glDepthFunc( GL_LESS );		 // depth-testing interprets a smaller value as "closer"
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS);		 // depth-testing interprets a smaller value as "closer"
 
-	/* OTHER STUFF GOES HERE NEXT */
-	//GLfloat points[] = { 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
-	//GLfloat colours[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+								 /* OTHER STUFF GOES HERE NEXT */
+								 //GLfloat points[] = { 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
+								 //GLfloat colours[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 
-	GLfloat vertices[] = { 0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 
-							0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 
-							-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-							0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,};
-	
-	GLint indice[] = { 0, 1, 2, 2, 1, 3};
+								 //criando vertices e cores numa só estrutura (x, y, z, r, g, b)
+	GLfloat vertices[] = { 0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, };
 
+	//0 é o primeiro vértice (topo do triangulo) e 3 é o ponto adicional para fazer um triangulo invertido
+	//ordem para renderização, sempre em sentido horário.
+	GLint indice[] = { 0, 1, 2, 2, 1, 3 };
+
+	//criando variável trans com rotação, escala e translação.
 	glm::mat4 trans;
 	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-	trans = glm::scale(trans, glm::vec3(1.5, 1.5, 1.5));
-	trans = glm::translate(trans, glm::vec3(0.25f, 0.25f, 0.0f));
+	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+	trans = glm::translate(trans, glm::vec3(0.3f, -0.3f, 0.0f));
 
+	//apenas um vbo
 	GLuint points_vbo;
-	glGenBuffers( 1, &points_vbo );
-	glBindBuffer( GL_ARRAY_BUFFER, points_vbo );
-	glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
+	glGenBuffers(1, &points_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	/* create a second VBO, containing the array of colours.
 	note that we could also put them both into a single vertex buffer. in this
 	case we would use the pointer and stride parameters of glVertexAttribPointer()
 	to describe the different data layouts */
-	
+
 	//GLuint colours_vbo;
 	//glGenBuffers( 1, &colours_vbo );
 	//glBindBuffer( GL_ARRAY_BUFFER, colours_vbo );
 	//glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
-	
+
 	GLuint ebo;
 	glGenBuffers(1, &ebo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
@@ -82,76 +87,79 @@ int main() {
 	respectively. we also have to explicitly enable both 'attribute' variables.
 	'attribute' is the older name for vertex shader 'in' variables. */
 	GLuint vao;
-	glGenVertexArrays( 1, &vao );
-	glBindVertexArray( vao );
-	glBindBuffer( GL_ARRAY_BUFFER, points_vbo );
-	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, 0 );
-	//usando somente um vbo.
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, 0);
+
+	//usando somente um vbo e definindo o uso dos dados.
 	//glBindBuffer( GL_ARRAY_BUFFER, colours_vbo);
-	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, (GLvoid*)(sizeof(GLfloat)*3));
-	glEnableVertexAttribArray( 0 );
-	glEnableVertexAttribArray( 1 );
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, (GLvoid*)(sizeof(GLfloat) * 3));
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 
 	char vertex_shader[1024 * 256];
 	char fragment_shader[1024 * 256];
-	parse_file_into_str( "test_vs.glsl", vertex_shader, 1024 * 256 );
-	parse_file_into_str( "test_fs.glsl", fragment_shader, 1024 * 256 );
+	parse_file_into_str("test_vs.glsl", vertex_shader, 1024 * 256);
+	parse_file_into_str("test_fs.glsl", fragment_shader, 1024 * 256);
 
-	GLuint vs = glCreateShader( GL_VERTEX_SHADER );
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
 	const GLchar *p = (const GLchar *)vertex_shader;
-	glShaderSource( vs, 1, &p, NULL );
-	glCompileShader( vs );
+	glShaderSource(vs, 1, &p, NULL);
+	glCompileShader(vs);
 
 	// check for compile errors
 	int params = -1;
-	glGetShaderiv( vs, GL_COMPILE_STATUS, &params );
-	if ( GL_TRUE != params ) {
-		fprintf( stderr, "ERROR: GL shader index %i did not compile\n", vs );
-		print_shader_info_log( vs );
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &params);
+	if (GL_TRUE != params) {
+		fprintf(stderr, "ERROR: GL shader index %i did not compile\n", vs);
+		print_shader_info_log(vs);
 		return 1; // or exit or something
 	}
 
-	GLuint fs = glCreateShader( GL_FRAGMENT_SHADER );
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
 	p = (const GLchar *)fragment_shader;
-	glShaderSource( fs, 1, &p, NULL );
-	glCompileShader( fs );
+	glShaderSource(fs, 1, &p, NULL);
+	glCompileShader(fs);
 
 	// check for compile errors
-	glGetShaderiv( fs, GL_COMPILE_STATUS, &params );
-	if ( GL_TRUE != params ) {
-		fprintf( stderr, "ERROR: GL shader index %i did not compile\n", fs );
-		print_shader_info_log( fs );
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &params);
+	if (GL_TRUE != params) {
+		fprintf(stderr, "ERROR: GL shader index %i did not compile\n", fs);
+		print_shader_info_log(fs);
 		return 1; // or exit or something
 	}
 
 	GLuint shader_programme = glCreateProgram();
-	glAttachShader( shader_programme, fs );
-	glAttachShader( shader_programme, vs );
-	glLinkProgram( shader_programme );
+	glAttachShader(shader_programme, fs);
+	glAttachShader(shader_programme, vs);
+	glLinkProgram(shader_programme);
 
-	glGetProgramiv( shader_programme, GL_LINK_STATUS, &params );
-	if ( GL_TRUE != params ) {
-		fprintf( stderr, "ERROR: could not link shader programme GL index %i\n",
-						 shader_programme );
-		print_programme_info_log( shader_programme );
+	glGetProgramiv(shader_programme, GL_LINK_STATUS, &params);
+	if (GL_TRUE != params) {
+		fprintf(stderr, "ERROR: could not link shader programme GL index %i\n",
+			shader_programme);
+		print_programme_info_log(shader_programme);
 		return false;
 	}
 
-	glEnable( GL_CULL_FACE ); // cull face
-	glCullFace( GL_BACK );		// cull back face
-	glFrontFace( GL_CW );			// GL_CCW for counter clock-wise
+	glEnable(GL_CULL_FACE); // cull face
+	glCullFace(GL_BACK);		// cull back face
+	glFrontFace(GL_CW);			// GL_CCW for counter clock-wise
 
-	while ( !glfwWindowShouldClose( g_window ) ) {
-		_update_fps_counter( g_window );
+	while (!glfwWindowShouldClose(g_window)) {
+		_update_fps_counter(g_window);
 		// wipe the drawing surface clear
-		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-		glViewport( 0, 0, g_gl_width, g_gl_height );
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glViewport(0, 0, g_gl_width, g_gl_height);
 
-		glUseProgram( shader_programme );
+		glUseProgram(shader_programme);
 
+		//aplicando as transformações.
 		unsigned int transformLoc = glGetUniformLocation(shader_programme, "transform");
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-		glBindVertexArray( vao );
+
+		glBindVertexArray(vao);
 		// draw points 0-3 from the currently bound VAO with current in-use shader
 		//glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices));
 
@@ -160,11 +168,11 @@ int main() {
 
 		// update other events like input handling
 		glfwPollEvents();
-		if ( GLFW_PRESS == glfwGetKey( g_window, GLFW_KEY_ESCAPE ) ) {
-			glfwSetWindowShouldClose( g_window, 1 );
+		if (GLFW_PRESS == glfwGetKey(g_window, GLFW_KEY_ESCAPE)) {
+			glfwSetWindowShouldClose(g_window, 1);
 		}
 		// put the stuff we've been drawing onto the display
-		glfwSwapBuffers( g_window );
+		glfwSwapBuffers(g_window);
 	}
 
 	// close GL context and any other GLFW resources
